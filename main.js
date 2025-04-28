a69dea2741394b76c62501854cf5fe1e5c029fcf
Heli135 <164352976+Hlm19966@users.noreply.github.com>
2025-04-28 14:29:58 +0330

    added main js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert("Hello world!")
+const mobileMenu = document.getElementById("mobileMenu");
+const iconMenu = document.getElementById("iconMenu");
+const closeIcon = document.getElementById("closeIcon");
+
+
+iconMenu.addEventListener("click", ()=> {
+    mobileMenu.classList.add('active');
+});
+
+
+closeIcon.addEventListener("click", ()=> {
+    mobileMenu.classList.remove('active');
+});
